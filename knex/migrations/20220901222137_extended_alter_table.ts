7c8e26f64a88fbaa2faf0cd02fdc8153d8ea14ac
Sebastian <jstraslavina@fgs.co>
2025-05-18 19:20:47 -0500

    Refactor :monster migration reintroduce 'name' column in Monster table and remove foreign key constraints from Battle table.

--- knex/migrations/20220901222137_extended_alter_table.ts
+++ knex/migrations/20220901222137_extended_alter_table.ts
@@ -2,31 +2,13 @@ import { Knex } from 'knex';
 import { Battle, Monster } from '../../src/models';
 
 export const up = async (knex: Knex): Promise<void> => {
-  await knex.schema.alterTable(Battle.tableName, (table) => {
-    table
-      .foreign('monsterA')
-      .references('id')
-      .inTable(Monster.tableName)
-      .onDelete('CASCADE');
-
-    table
-      .foreign('monsterB')
-      .references('id')
-      .inTable(Monster.tableName)
-      .onDelete('CASCADE');
-
-    table
-      .foreign('winner')
-      .references('id')
-      .inTable(Monster.tableName)
-      .onDelete('SET NULL');
+  await knex.schema.alterTable(Monster.tableName, (table: Knex.TableBuilder) => {
+    table.string('name').notNullable();
   });
 };
 
 export const down = async (knex: Knex): Promise<void> => {
-  await knex.schema.alterTable(Battle.tableName, (table) => {
-    table.dropForeign(['monsterA']);
-    table.dropForeign(['monsterB']);
-    table.dropForeign(['winner']);
+  await knex.schema.alterTable(Monster.tableName, (table: Knex.TableBuilder) => {
+    table.dropColumn('name');
   });
 };
